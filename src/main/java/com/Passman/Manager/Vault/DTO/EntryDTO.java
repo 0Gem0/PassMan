c20caaadbaa75ee8988c5afd3b3178049659f299
Alereo <gmullayarov@mail.ru>
2025-12-23 22:56:48 +0300

    fixed get entries

--- src/main/java/com/Passman/Manager/Vault/DTO/EntryDTO.java
+++ src/main/java/com/Passman/Manager/Vault/DTO/EntryDTO.java
@@ -7,20 +7,25 @@ import lombok.Setter;
 @Getter
 @Setter
     public class EntryDTO {
+
+        private long id;
         private String title;
         private String website;
         private String email;
         private String categoryName;
         private String data;
 
-        public EntryDTO(String title, String website, String email, String categoryName, String data) {
-            this.title = title;
-            this.website = website;
-            this.email = email;
-            this.categoryName = categoryName;
-            this.data = data;
-        }
-        public EntryDTO(){
+
+    public EntryDTO(long id, String title, String website, String email, String categoryName, String data) {
+        this.id = id;
+        this.title = title;
+        this.website = website;
+        this.email = email;
+        this.categoryName = categoryName;
+        this.data = data;
+    }
+
+    public EntryDTO(){
 
         }
     }
